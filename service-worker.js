b71d466536e9e5a5952c63b2baebf75fe134bf0a
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2026-06-03 21:17:24 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,23 @@
-const CACHE_NAME = 'clientes-cache-v1';
+const CACHE_NAME = 'marmitapay-v1';
 const urlsToCache = [
-  '/',
-  '/css/estilo.css',
-  '/manifest.json',
-  // adicione outros recursos que deseja armazenar em cache
+  './',
+  './index.html',
+  './style.css',
+  './app.js'
 ];
 
+// Instala o service worker e faz o cache dos arquivos
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Intercepta requisições para servir os arquivos do cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
